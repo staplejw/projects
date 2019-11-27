03de35106b369aafd21784c4e6742caca6af8416
Justin <jstaples.496@gmail.com>
2019-11-27 00:49:25 -0500

    changing from float to double and adding control over orbit point

--- Mandelbrot Set Visualizer/mandelbrot.cu
+++ Mandelbrot Set Visualizer/mandelbrot.cu
@@ -15,9 +15,9 @@ Usage:
 
 using namespace thrust;
 
-typedef struct {float real; float imag;} point; // complex numbers
+typedef struct {double real; double imag;} point; // complex numbers
 
-int MAX_ITER = 255; 
+int MAX_ITER = 500; 
 int W = 1024; // image dimensions
 int H = 512;
 int aspect_ratio = W / H;
@@ -25,11 +25,16 @@ int block_dim_x = 4; // 4 * 4 = 16 threads per block
 int block_dim_y = 4;
 dim3 block(block_dim_x, block_dim_y); // block and grid dimsions
 dim3 grid(W / block_dim_x, H / block_dim_y);
-float height = 1;
-float width = height * aspect_ratio;
-float scale = 1;
+double height = 1;
+double width = height * aspect_ratio;
+double scale = 1;
 point center;
 point bottom_left;
+
+double orbitReal = 0.0;
+double orbitImaginary = 0.0;
+double * orbitRealRef = &orbitReal;
+double * orbitImaginaryRef = &orbitImaginary;
 
 int * d_out;
 int * h_out;
@@ -39,22 +44,22 @@ const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
 // takes an integer respresenting the number of iterations and uses it to assign RGB values. RGBA data can be exactly stored in a 4-byte int
 __device__ int assignColour(int i) {
 	int val = 0;
-	val += powf(i, 2); // blue
+	// val += powf(i, 1); // blue
 	val = val << 8; // each integer is 4 bytes but each colour component is only 1, so shift them to the correct positions (RGBA)
 	val += powf(i, 2); // green
 	val = val << 8;
-	val += powf(i, 2); // red
+	// val += powf(i, 1); // red
 
 	return val;
 }
 
 // given a complex point C and an upper bound, determine the number of iterations until divergence
-__device__ int iterations(complex<float> C, int max) {
-	complex<float> z0(0, 0); //  mandelbrot set defined for z0 = 0 + 0i
-	complex<float> zn; // populated during iteration
+__device__ int numberOfIterations(complex<double> orbit, complex<double> C, int max) {
+	complex<double> z0 = orbit; // orbit point, for mandelbrot z0 = 0 + 0i
+	complex<double> zn; // populated during iteration
 
 	int iter = 0;
-	float mag = 0;	
+	double mag = 0;	
 	while (mag < 2 && iter < max) { // ensure the magnitude is bounded by 2
 		mag = abs(z0);
 		zn = z0 * z0 + C;
@@ -65,19 +70,19 @@ __device__ int iterations(complex<float> C, int max) {
 }
 
 // process each pixel/thread individually
-__global__ void process(int * d_out, point p, float w, float h, float s, int max){
+__global__ void process(int * d_out, point p, double w, double h, double s, int max, complex<double> orbit){
 	// map block and grid coordinates to pixel coordinates
 	int y_idx = 4 * blockIdx.y + threadIdx.y;
 	int x_idx = 4 * blockIdx.x + threadIdx.x;
 	int idx = 1024 * y_idx + x_idx;
 
 	// map pixel coordinates to real and imaginary components
-	float R = p.real + (x_idx / 1023.0) * w;
-	float I = p.imag + (y_idx / 511.0) * h;
+	double R = p.real + (x_idx / 1023.0) * w;
+	double I = p.imag + (y_idx / 511.0) * h;
 
 	// determine number of iterations for each complex point, bounded the maximum number of iterations
-	complex<float> C(R, I); // each thread is responsible for 1 complex point P
-	int iter = iterations(C, max);
+	complex<double> C(R, I); // each thread is responsible for 1 complex point P
+	int iter = numberOfIterations(orbit, C, max);
 	
 	// use number of iterations to assign colours to each point
 	int val = assignColour(iter);
@@ -91,7 +96,8 @@ void display() {
     glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
 
-	process<<<grid, block>>>(d_out, bottom_left, width, height, scale, MAX_ITER);
+	complex<double> orbit(*orbitRealRef, *orbitImaginaryRef);
+	process<<<grid, block>>>(d_out, bottom_left, width, height, scale, MAX_ITER, orbit);
 
 	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
@@ -117,6 +123,18 @@ void keyboard(unsigned char key, int x, int y) {
 			width *= 1.07;
 			bottom_left.real = center.real - width / 2.0;
 			bottom_left.imag = center.imag - height / 2.0;
+			break;
+		case 'i':
+			orbitImaginary += 0.01;
+			break;
+		case 'k':
+			orbitImaginary -= 0.01;
+			break;
+		case 'j':
+			orbitReal -= 0.01;
+			break;
+		case 'l':
+			orbitReal += 0.01;
 			break;
 	}
 }
